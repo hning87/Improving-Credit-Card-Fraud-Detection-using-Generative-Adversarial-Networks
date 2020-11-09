1aa49cc79e51883b4409ab597c3f6eb55635ca38
JunYing0306 <49451765+JunYing0306@users.noreply.github.com>
2020-11-09 15:28:41 -0500

    Update GAN Visualization.py

--- code/GAN Visualization.py
+++ code/GAN Visualization.py
@@ -15,6 +15,8 @@ generator = tf.keras.models.load_model('gan_pre_generator.h5')
 # generate fraud data
 gen_1000 = utils.gen_data(generator, 1000)
 df_gen = pd.DataFrame(data=gen_1000, index=None, columns=fraud.columns)
+print(df_gen.describe())
 
+# compare with real data
 utils.boxplot_compare(fraud, df_gen, 'green', 'purple', 'GAN with Pre-Train',
                       'Original Data Distribution versus GAN with Pre-Train')
