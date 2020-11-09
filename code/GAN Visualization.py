f5286cf4965ac1e6f8802e5fb9a2dfe3a07ff54b
JunYing0306 <49451765+JunYing0306@users.noreply.github.com>
2020-11-09 15:15:49 -0500

    Add files via upload

---
+++ code/GAN Visualization.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import tensorflow as tf
+import utils
+
+# load data
+df = pd.read_csv('creditcard.csv')
+
+# data preprocessing
+data, df_fraud = utils.preprocessing(df)
+fraud = df_fraud.loc[:, df.columns != 'Class']
+
+# load generator
+generator = tf.keras.models.load_model('gan_pre_generator.h5')
+
+# generate fraud data
+gen_1000 = utils.gen_data(generator, 1000)
+df_gen = pd.DataFrame(data=gen_1000, index=None, columns=fraud.columns)
+
+utils.boxplot_compare(fraud, df_gen, 'green', 'purple', 'GAN with Pre-Train',
+                      'Original Data Distribution versus GAN with Pre-Train')
